17664b3916ac75b97b5ffedc833c8d103c7a012a
murderteeth <murdertxxth@gmail.com>
2024-11-23 22:35:25 +0000

    Fix vaultStrategies resolver

--- packages/web/app/api/gql/resolvers/vaultStrategies.ts
+++ packages/web/app/api/gql/resolvers/vaultStrategies.ts
@@ -13,16 +13,20 @@ const vaultStrategies = async (_: any, args: { chainId: number, vault: string })
 
     SELECT 
       thing.chain_id,
-      thing.address,
-      thing.defaults,
-      snapshot.snapshot,
-      snapshot.hook
+      thing.address
     FROM thing
     JOIN snapshot 
       ON thing.chain_id = snapshot.chain_id
       AND thing.address = snapshot.address
     WHERE thing.chain_id = $1
       AND thing.address = ANY(SELECT * FROM strategies)
+      AND (
+        COALESCE(thing.defaults->>'erc4626', 'false')::boolean = true
+        AND (
+          (COALESCE(thing.defaults->>'yearn', 'false')::boolean = false)
+          OR (COALESCE(thing.defaults->>'yearn', 'false')::boolean = true AND thing.label = 'vault')
+        )
+      )
     ORDER BY snapshot.hook->>'totalDebtUsd' DESC;`,
     [chainId, vault])
 
