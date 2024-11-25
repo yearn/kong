e64966fc9400b98f355d491f1652537ddd90eeb3
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-11-25 17:38:44 -0500

    Add hook for AddedNewVault event (#195)

--- packages/ingest/abis/yearn/3/roleManagerFactory/event/hook.ts
+++ packages/ingest/abis/yearn/3/roleManagerFactory/event/hook.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 import { mq } from 'lib'
 import { toEventSelector } from 'viem'
 import { EvmAddressSchema, ThingSchema, zhexstring } from 'lib/types'
+import { getBlock } from 'lib/blocks'
 
 export const topics = [
   `event NewProject(bytes32 indexed projectId, address indexed roleManager)`
@@ -13,13 +14,18 @@ export default async function process(chainId: number, address: `0x${string}`, d
     roleManager: EvmAddressSchema
   }).parse(data.args)
 
+  const { 
+    number: inceptBlock, 
+    timestamp: inceptTime 
+  } = await getBlock(chainId, data.blockNumber)
+
   await mq.add(mq.job.load.thing, ThingSchema.parse({
     chainId, address: roleManager, label: 'roleManager',
     defaults: { 
       roleManagerFactory: address, 
       project: { id: projectId }, 
-      inceptBlock: data.blockNumber, 
-      inceptTime: data.timestamp 
+      inceptBlock, 
+      inceptTime
     }
   }))
 }
