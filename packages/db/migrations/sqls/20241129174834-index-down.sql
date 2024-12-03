d0aceb499080a869c47857da0f99ce97f28bf180
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-12-03 17:46:35 -0500

    Index for bulk output queries (#201)
    * Add output and thing indexes for bulk query
    
    * Monitor thing counts flexibly

---
+++ packages/db/migrations/sqls/20241129174834-index-down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_output_chain_id_address;
+DROP INDEX IF EXISTS idx_output_label_component_block_time;
+DROP INDEX IF EXISTS idx_thing_chain_id_address_defaults;
--- packages/web/components/Things.tsx
+++ packages/web/components/Things.tsx
@@ -4,16 +4,14 @@ import { z } from 'zod'
 import Frosty from './Frosty'
 import { useData } from '@/hooks/useData'
 import LineItem, { formatLineItemValue } from './LineItem'
-import { useEffect } from 'react'
+import { useMemo } from 'react'
 
 const IndexStatsSchema = z.object({
-  thing_total: z.number().optional().default(0),
-  thing_vault_total: z.number().optional().default(0),
-  thing_strategy_total: z.number().optional().default(0),
-  thing_erc20_total: z.number().optional().default(0),
-  thing_debtallocator_total: z.number().optional().default(0),
-  thing_accountant_total: z.number().optional().default(0),
-  thing_tradehandler_total: z.number().optional().default(0),
+  things: z.array(z.object({
+    label: z.string(),
+    count: z.number({ coerce: true })
+  })),
+  things_total: z.number().optional().default(0),
   output_total: z.number().optional().default(0),
   evmlog_total: z.number().optional().default(0),
   eventCounts: z.array(z.object({
@@ -24,22 +22,29 @@ const IndexStatsSchema = z.object({
 
 export default function Things() {
   const { monitor } = useData()
-  const { indexStatsJson } = monitor
-  const indexStats = IndexStatsSchema.parse(JSON.parse(indexStatsJson))
 
-  const lineItems = [
-    { label: "vaults", value: indexStats.thing_vault_total },
-    { label: "strategies", value: indexStats.thing_strategy_total },
-    { label: "erc20s", value: indexStats.thing_erc20_total },
-    { label: "debt allocators", value: indexStats.thing_debtallocator_total },
-    { label: "accountants", value: indexStats.thing_accountant_total },
-    { label: "trade handlers", value: indexStats.thing_tradehandler_total },
-  ].sort((a, b) => b.value - a.value)
+  const indexStats = useMemo(() => {
+    const { indexStatsJson } = monitor
+    const json = JSON.parse(indexStatsJson)
+    const things = Object.keys(json).filter(key => key.startsWith('thing_')).map(key => ({
+      label: key.replace('thing_', '').replace('_total', ''),
+      count: json[key]
+    }))
+    return IndexStatsSchema.parse({ ...json, things, things_total: json.thing_total })
+  }, [monitor])
+
+  const lineItems = useMemo(() => {
+    const result = indexStats.things.filter(thing => thing.label !== 'total').map(thing => ({ 
+      label: thing.label, 
+      value: thing.count 
+    })).sort((a, b) => b.value - a.value)
+    return result
+  }, [indexStats])
 
   return <div className={'w-full flex flex-col items-start'}>
     <div className="w-full flex items-center justify-between">
       <div className="font-bold text-lg">Things</div>
-      <Frosty _key={`thing_total-${indexStats.thing_total}`} disabled={indexStats.thing_total < 1}>{formatLineItemValue(indexStats.thing_total)}</Frosty>
+      <Frosty _key={`thing_total-${indexStats.things_total}`} disabled={indexStats.things_total < 1}>{formatLineItemValue(indexStats.things_total)}</Frosty>
     </div>
     {lineItems.map(({ label, value }) => (
       <LineItem key={label} label={label} value={value} />
