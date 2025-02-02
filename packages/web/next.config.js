02e0a279cdb27174031296084e6d852b5a7f1906
matheus1lva <matheus.g.silva@hotmail.com>
2025-02-02 11:42:13 -0300

    cleanup

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@lib"],
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
