a6ceebfea12056dee85b2170339230007e28a80b
murderteeth <murdertxxth@gmail.com>
2025-02-04 22:37:32 +0000

    Lint terminal

--- packages/terminal/menu/tools.ts
+++ packages/terminal/menu/tools.ts
@@ -30,28 +30,28 @@ async function action() {
 
   if (confirm) {
     switch(tool) {
-      case 'flush-failed-jobs': {
-        const keys = [...Object.keys(mq.q), ...chains.map(chain => `extract-${chain.id}`)]
-        for(const key of keys) {
-          const queue = mq.connect(key)
-          await queue.clean(0, Number.MAX_SAFE_INTEGER, 'failed')
-          await queue.close()
-        }
-        break
+    case 'flush-failed-jobs': {
+      const keys = [...Object.keys(mq.q), ...chains.map(chain => `extract-${chain.id}`)]
+      for(const key of keys) {
+        const queue = mq.connect(key)
+        await queue.clean(0, Number.MAX_SAFE_INTEGER, 'failed')
+        await queue.close()
       }
+      break
+    }
 
-      case 'flush-redis': {
-        const client = createClient({
-          url: `redis://${process.env.REDIS_HOST || 'localhost'}:${(process.env.REDIS_PORT || 6379) as number}`
-        })
-        await client.connect()
-        await client.flushAll()
-        await client.quit()
-        break
-      }
+    case 'flush-redis': {
+      const client = createClient({
+        url: `redis://${process.env.REDIS_HOST || 'localhost'}:${(process.env.REDIS_PORT || 6379) as number}`
+      })
+      await client.connect()
+      await client.flushAll()
+      await client.quit()
+      break
+    }
 
-      case 'reset-database': {
-        await db.query(`
+    case 'reset-database': {
+      await db.query(`
           TRUNCATE TABLE evmlog;
           TRUNCATE TABLE evmlog_strides;
           TRUNCATE TABLE thing;
@@ -59,20 +59,20 @@ async function action() {
           TRUNCATE TABLE output;
           TRUNCATE TABLE price;
         `)
-        await db.query('VACUUM FULL evmlog;')
-        await db.query('REINDEX TABLE evmlog;')
-        await db.query('VACUUM FULL evmlog_strides;')
-        await db.query('REINDEX TABLE evmlog_strides;')
-        await db.query('VACUUM FULL thing;')
-        await db.query('REINDEX TABLE thing;')
-        await db.query('VACUUM FULL snapshot;')
-        await db.query('REINDEX TABLE snapshot;')
-        await db.query('VACUUM FULL output;')
-        await db.query('REINDEX TABLE output;')
-        await db.query('VACUUM FULL price;')
-        await db.query('REINDEX TABLE price;')
-        break
-      }
+      await db.query('VACUUM FULL evmlog;')
+      await db.query('REINDEX TABLE evmlog;')
+      await db.query('VACUUM FULL evmlog_strides;')
+      await db.query('REINDEX TABLE evmlog_strides;')
+      await db.query('VACUUM FULL thing;')
+      await db.query('REINDEX TABLE thing;')
+      await db.query('VACUUM FULL snapshot;')
+      await db.query('REINDEX TABLE snapshot;')
+      await db.query('VACUUM FULL output;')
+      await db.query('REINDEX TABLE output;')
+      await db.query('VACUUM FULL price;')
+      await db.query('REINDEX TABLE price;')
+      break
+    }
     }
   }
 }
