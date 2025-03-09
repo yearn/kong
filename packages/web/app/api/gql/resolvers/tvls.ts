eafb58a7b28d06a0829d197b7a91188e8d9302cc
matheus1lva <matheus.g.silva@hotmail.com>
2025-03-09 13:57:53 -0300

    fix: tvl shows value as null

--- packages/web/app/api/gql/resolvers/tvls.ts
+++ packages/web/app/api/gql/resolvers/tvls.ts
@@ -47,7 +47,7 @@ const tvls = async (_: object, args: {
       t.period,
       t.block_number,
       t.time,
-      COALESCE(p.price_usd, 0) AS price_usd,
+      CASE WHEN p.price_usd = 0 THEN NULL ELSE p.price_usd END AS price_usd,
       COALESCE(p.price_source, 'na') AS price_source
     FROM tvl_data t
     LEFT JOIN price p
