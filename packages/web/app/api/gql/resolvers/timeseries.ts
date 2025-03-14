131d4dc874892dc202be2bea59b41efff19130e7
matheus1lva <matheus.g.silva@hotmail.com>
2025-03-14 13:21:12 -0300

    smol fix

--- packages/web/app/api/gql/resolvers/timeseries.ts
+++ packages/web/app/api/gql/resolvers/timeseries.ts
@@ -37,7 +37,7 @@ async function alltimeseries(args: {
       address,
       CAST($3 AS text) AS label,
       COALESCE(CAST($4 AS text), component) AS component,
-      NULLIF(value, 0) AS value,
+      COALESCE(AVG(NULLIF(value, 0)), 0) AS value,
       CAST($5 AS text) AS period,
       time_bucket(CAST($5 AS interval), block_time) AS time
     FROM output
@@ -46,7 +46,7 @@ async function alltimeseries(args: {
       AND label = $3
       AND (component = $4 OR $4 IS NULL)
       AND (block_time > to_timestamp($7) OR $7 IS NULL)
-    GROUP BY chain_id, address, component, time, value
+    GROUP BY chain_id, address, component, time
     ORDER BY time ASC
     LIMIT $6`,
   [chainId, address, label, component, period ?? '1 day', limit ?? 100, timestamp])
@@ -68,7 +68,7 @@ async function yearntimeseries(args: {
       output.address,
       CAST($3 AS text) AS label,
       COALESCE(CAST($4 AS text), output.component) AS component,
-      NULLIF(output.value, 0) AS value,
+      COALESCE(AVG(NULLIF(output.value, 0)), 0) AS value,
       CAST($5 AS text) AS period,
       time_bucket(CAST($5 AS interval), output.block_time) AS time
     FROM output
@@ -81,7 +81,7 @@ async function yearntimeseries(args: {
       AND output.label = $3
       AND (output.component = $4 OR $4 IS NULL)
       AND (output.block_time > to_timestamp($7) OR $7 IS NULL)
-    GROUP BY output.chain_id, output.address, output.component, time, output.value
+    GROUP BY output.chain_id, output.address, output.component, time
     ORDER BY time ASC
     LIMIT $6`,
   [chainId, address, label, component, period ?? '1 day', limit ?? 100, timestamp])
