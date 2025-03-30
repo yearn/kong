4ae00a63babd88b574d156e9b962722ed029acae
murderteeth <89237203+murderteeth@users.noreply.github.com>
2025-03-30 17:23:29 -0400

    Get usr x pass from envars (#228)

--- packages/web/app/api/mq/bull.ts
+++ packages/web/app/api/mq/bull.ts
@@ -3,6 +3,8 @@ import { Queue } from 'bullmq'
 export const bull = { connection: {
   host: process.env.BULLMQ_REDIS_HOST || 'localhost',
   port: (process.env.BULLMQ_REDIS_PORT || 6379) as number,
+  username: process.env.BULLMQ_REDIS_USERNAME || undefined,
+  password: process.env.BULLMQ_REDIS_PASSWORD || undefined
 }}
 
 export function getQueue(queues: Record<string, Queue>, q: string) {
