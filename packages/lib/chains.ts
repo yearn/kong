0da0371aa2352c56b951a51bd68d57a0c50ab7a7
murderteeth <89237203+murderteeth@users.noreply.github.com>
2025-10-25 18:37:56 -0400

    Remove mode chain (#267)
    * Remove mode chain
    
    * Filter abi things by origin instead of flag

--- packages/lib/chains.ts
+++ packages/lib/chains.ts
@@ -31,29 +31,6 @@ export const customChains = {
     testnet: false,
   }),
 
-  mode: /*#__PURE__*/ defineChain({
-    id: 34443,
-    name: 'Mode Mainnet',
-    nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
-    rpcUrls: {
-      default: {
-        http: ['https://mainnet.mode.network'],
-      },
-    },
-    blockExplorers: {
-      default: {
-        name: 'Mode Explorer',
-        url: 'https://explorer.mode.network',
-      },
-    },
-    contracts: {
-      multicall3: {
-        address: '0xca11bde05977b3631167028862be2a173976ca11',
-        blockCreated: 2465882,
-      },
-    },
-  }),
-
   bera: /*#__PURE__*/ defineChain({
     id: 80094,
     name: 'Berachain',
