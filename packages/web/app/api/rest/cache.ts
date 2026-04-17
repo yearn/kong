368acc49af0e8ac241d7a80bc1a8d330bf2f9c7a
matheus1lva <matheus.g.silva@hotmail.com>
2026-04-17 18:21:18 -0300

    fix

--- packages/web/app/api/rest/cache.ts
+++ packages/web/app/api/rest/cache.ts
@@ -83,6 +83,6 @@ export async function cacheMSet(pairs: Array<[string, string]>): Promise<void> {
 }
 
 export async function disconnect(): Promise<void> {
-  if (writeClient.isOpen) await writeClient.disconnect()
+  await writeClient.disconnect()
   await keyv.disconnect()
 }
